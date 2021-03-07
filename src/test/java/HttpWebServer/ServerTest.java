75ca4df1f34830e9f1a7e23de39b872f31af01b3
Marcel <unka@itu.dk>
2021-03-07 16:38:40 +0100

    added javadoc to ServerTest.java

--- src/test/java/HttpWebServer/ServerTest.java
+++ src/test/java/HttpWebServer/ServerTest.java
@@ -15,6 +15,9 @@ import java.net.http.HttpClient.Version;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.nio.file.Paths;
 
+/**
+ * A unit test which tests the basic functionalities of the Server class and its inner classes. 
+ */
 @TestInstance(Lifecycle.PER_CLASS)
 public class ServerTest {
 
@@ -28,110 +31,91 @@ public class ServerTest {
     }
 
     @Test
-    void getRootTest() {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(baseURL)).header("Content-Type", "txt/html")
-                .version(Version.HTTP_1_1).GET().build();
+    void getRootDirectoryTest() {
+        HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(baseURL))
+                                .header("Content-Type", "txt/html")
+                                .version(Version.HTTP_1_1)
+                                .GET()
+                                .build();
 
-        HttpResponse<Void> response = null;
-        try {
-            response = client.send(request, HttpResponse.BodyHandlers.discarding());
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-
+        HttpResponse<Void> response = getResponse(request);
         assertEquals(200, response.statusCode());
     }
 
     @Test
     void successfulHeadRequestTest() {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(baseURL + "src/test/resources/existing.txt"))
-                .header("Content-Type", "text/plain").version(Version.HTTP_1_1)
-                .method("HEAD", HttpRequest.BodyPublishers.noBody()).build();
-
-        HttpResponse<Void> response = null;
-        try {
-            response = client.send(request, HttpResponse.BodyHandlers.discarding());
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(baseURL + "src/test/resources/existing.txt"))
+                                .header("Content-Type", "text/plain")
+                                .version(Version.HTTP_1_1)
+                                .method("HEAD", HttpRequest.BodyPublishers.noBody())
+                                .build();
+        
+        HttpResponse<Void> response = getResponse(request);
         assertEquals(200, response.statusCode());
     }
 
     @Test
     void unsuccessfulHeadRequestTest() {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(baseURL + "src/test/resources/notExisting.txt"))
-                .header("Content-Type", "text/plain").version(Version.HTTP_1_1)
-                .method("HEAD", HttpRequest.BodyPublishers.noBody()).build();
+        HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(baseURL + "src/test/resources/notExisting.txt"))
+                                .header("Content-Type", "text/plain")
+                                .version(Version.HTTP_1_1)
+                                .method("HEAD", HttpRequest.BodyPublishers.noBody())
+                                .build();
 
-        HttpResponse<Void> response = null;
-        try {
-            response = client.send(request, HttpResponse.BodyHandlers.discarding());
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        HttpResponse<Void> response = getResponse(request);
         assertEquals(404, response.statusCode());
     }
 
     @Test
     void successfulGetRequestFolderTest() {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(baseURL + "src/test/resources/existing/"))
-                .version(Version.HTTP_1_1).GET().build();
+        HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(baseURL + "src/test/resources/existing/"))
+                                .version(Version.HTTP_1_1)
+                                .GET()
+                                .build();
 
-        HttpResponse<Void> response = null;
-        try {
-            response = client.send(request, HttpResponse.BodyHandlers.discarding());
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        HttpResponse<Void> response = getResponse(request);
         assertEquals(200, response.statusCode());
     }
 
     @Test
     void successfulGetRequestFileTest() {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(baseURL + "src/test/resources/existing.txt"))
-                .header("Content-Type", "text/plain").version(Version.HTTP_1_1).GET().build();
+        HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(baseURL + "src/test/resources/existing.txt"))
+                                .header("Content-Type", "text/plain")
+                                .version(Version.HTTP_1_1)
+                                .GET()
+                                .build();
 
-        HttpResponse<Void> response = null;
-        try {
-            response = client.send(request, HttpResponse.BodyHandlers.discarding());
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        HttpResponse<Void> response = getResponse(request);
         assertEquals(200, response.statusCode());
     }
 
     @Test
     void unsuccessfulGetRequestFolderTest() {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(baseURL + "src/test/resources/notExisting/"))
-                .version(Version.HTTP_1_1).GET().build();
+        HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(baseURL + "src/test/resources/notExisting/"))
+                                .version(Version.HTTP_1_1)
+                                .GET()
+                                .build();
 
-        HttpResponse<Void> response = null;
-        try {
-            response = client.send(request, HttpResponse.BodyHandlers.discarding());
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        HttpResponse<Void> response = getResponse(request);
         assertEquals(404, response.statusCode());
     }
 
     @Test
     void unsuccessfulGetRequestFileTest() {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(baseURL + "src/test/resources/notExisting.txt"))
-                .header("Content-Type", "text/plain").version(Version.HTTP_1_1).GET().build();
+        HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(baseURL + "src/test/resources/notExisting.txt"))
+                                .header("Content-Type", "text/plain")
+                                .version(Version.HTTP_1_1)
+                                .GET()
+                                .build();
 
-        HttpResponse<Void> response = null;
-        try {
-            response = client.send(request, HttpResponse.BodyHandlers.discarding());
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        HttpResponse<Void> response = getResponse(request);
         assertEquals(404, response.statusCode());
     }
 
@@ -140,16 +124,37 @@ public class ServerTest {
         HttpClient client = HttpClient.newHttpClient();
         HttpResponse<Void> response = null;
         try {
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(baseURL)).header("Content-Type", "text/plain")
-                    .version(Version.HTTP_1_1).PUT(BodyPublishers.ofFile(Paths.get("src/test/resources/existing.txt")))
-                    .build();
+            HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(baseURL))
+                                .header("Content-Type", "text/plain")
+                                .version(Version.HTTP_1_1)
+                                .PUT(BodyPublishers.ofFile(Paths.get("src/test/resources/existing.txt")))
+                                .build();
             do {
                 response = client.send(request, HttpResponse.BodyHandlers.discarding());
             } while (response == null);
-
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
         assertEquals(405, response.statusCode());
+    }
+
+    /**
+     * A helper method which sends the request to the server 
+     * and returns the response to the calling method.
+     * 
+     * @param request The request which is send to the server.
+     * @return Returns the response from the server.
+     */
+    private HttpResponse<Void> getResponse(HttpRequest request) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<Void> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.discarding());
+        } catch (IOException | InterruptedException e) {
+            // Here: Create a log of the exception and the state of the system
+            e.printStackTrace();
+        }
+        return response;
     }
 }
